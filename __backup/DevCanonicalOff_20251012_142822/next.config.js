c195e76aaff9af2f371dc1c0bcd96bb32f9a1da0
CoreVistaTAKE <development@corevista-japan.com>
2025-11-18 16:44:53 +0900

    chore(repo): stop tracking backups/temp/artifacts; harden .gitignore

--- __backup/DevCanonicalOff_20251012_142822/next.config.js
+++
@@ -1,6 +0,0 @@
-﻿/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: { ignoreDuringBuilds: true },     // ← 一時的に Lint で落とさない
-  typescript: { ignoreBuildErrors: false }, // 型は通す（必要なら true にできる）
-};
-module.exports = nextConfig;
--- tmp/qr.js
+++
@@ -1,5 +0,0 @@
-﻿const QRCode = require("qrcode");
-const url = process.argv[2];
-QRCode.toDataURL(url, { errorCorrectionLevel:"M", scale:6, margin:1 })
-  .then(s=>process.stdout.write(s))
-  .catch(e=>{process.stderr.write(String(e)); process.exit(1);});
